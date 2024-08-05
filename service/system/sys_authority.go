c0422cd8f37dc0865f6f0bf572c7fbfc94c766ed
jelly <huguodong1212@gmail.com>
2024-08-05 11:20:48 +0800

    1.角色权限相关代码

--- service/system/sys_authority.go
+++ service/system/sys_authority.go
@@ -3,6 +3,7 @@ package system
 import (
 	"errors"
 	"gin-vue-admin/global"
+	"gin-vue-admin/model/common/request"
 	"gin-vue-admin/model/system"
 	systemReq "gin-vue-admin/model/system/request"
 	"gin-vue-admin/model/system/response"
@@ -57,4 +58,171 @@ func (authorityService *AuthorityService) CopyAuthority(copyInfo response.SysAut
 		return authority, ErrRoleExistence
 	}
 	copyInfo.Authority.Children = []system.SysAuthority{}
+	menus, err := MenuServiceApp.GetMenuAuthority(&request.GetAuthorityId{AuthorityId: copyInfo.OldAuthorityID})
+	if err != nil {
+		return
+	}
+	var baseMenu []system.SysBaseMenu
+	for _, v := range menus {
+		intNum := v.MenuID
+		v.SysBaseMenu.ID = uint(intNum)
+		baseMenu = append(baseMenu, v.SysBaseMenu)
+	}
+	copyInfo.Authority.SysBaseMenus = baseMenu
+	err = global.GVA_DB.Create(&copyInfo.Authority).Error
+	if err != nil {
+		return
+	}
+	var btns []system.SysAuthorityBtn
+	err = global.GVA_DB.Find(&btns, "authority_id = ?", copyInfo.OldAuthorityID).Error
+	if err != nil {
+		return
+	}
+	if len(btns) > 0 {
+		for i := range btns {
+			btns[i].AuthorityID = copyInfo.Authority.AuthorityID
+		}
+		err = global.GVA_DB.Create(&btns).Error
+		if err != nil {
+			return
+		}
+	}
+	paths := CasbinServiceApp.GetPolicyPathByAuthorityID(copyInfo.OldAuthorityID)
+	err = CasbinServiceApp.UpdateCasbin(copyInfo.Authority.AuthorityID, paths)
+	if err != nil {
+		_ = authorityService.DeleteAuthority(&copyInfo.Authority)
+	}
+	return copyInfo.Authority, err
+}
+
+// UpdateAuthority
+// @function: UpdateAuthority
+// @description: 更改一个角色
+// @param: auth model.SysAuthority
+// @return: authority system.SysAuthority, err error
+func (menuService *MenuService) UpdateAuthority(auth system.SysAuthority) (authority system.SysAuthority, err error) {
+	var oldAuthority system.SysAuthority
+	err = global.GVA_DB.Where("authority_id = ?", auth.AuthorityID).First(&oldAuthority).Error
+	if err != nil {
+		global.GVA_LOG.Debug(err.Error())
+		return system.SysAuthority{}, errors.New("查询角色数据失败")
+	}
+	err = global.GVA_DB.Model(&oldAuthority).Updates(&auth).Error
+	return auth, err
+}
+
+// DeleteAuthority
+// @function: DeleteAuthority
+// @description: 删除角色
+// @param: auth *model.SysAuthority
+// @return: err error
+func (authorityService *AuthorityService) DeleteAuthority(auth *system.SysAuthority) error {
+	if errors.Is(global.GVA_DB.Debug().Preload("Users").First(&auth).Error, gorm.ErrRecordNotFound) {
+		return errors.New("该角色不存在")
+	}
+	if len(auth.Users) != 0 {
+		return errors.New("此角色有用户正在使用禁止删除")
+	}
+	if !errors.Is(global.GVA_DB.Where("authority_id = ?", auth.AuthorityID).First(&system.SysUser{}).Error, gorm.ErrRecordNotFound) {
+		return errors.New("此角色有用户正在使用，禁止删除")
+	}
+	if !errors.Is(global.GVA_DB.Where("parent_id = ?", auth.ParentID).First(&system.SysAuthority{}).Error, gorm.ErrRecordNotFound) {
+		return errors.New("此角色存在子角色，不允许删除")
+	}
+	return global.GVA_DB.Transaction(func(tx *gorm.DB) error {
+		var err error
+		if err = tx.Preload("SysBaseMenus").Preload("DataAuthorityID").Where("authority_id = ?", auth.AuthorityID).First(auth).Unscoped().Delete(auth).Error; err != nil {
+			return err
+		}
+		if len(auth.SysBaseMenus) > 0 {
+			if err = tx.Model(auth).Association("SysBaseMenus").Delete(auth.SysBaseMenus); err != nil {
+				return err
+			}
+		}
+		if len(auth.DataAuthorityID) > 0 {
+			if err = tx.Model(&auth).Association("DataAuthorityID").Delete(auth.DataAuthorityID); err != nil {
+				return err
+			}
+		}
+		if err = tx.Delete(&system.SysUserAuthority{}, "sys_authority_authority_id = ?", auth.AuthorityID).Error; err != nil {
+			return err
+		}
+		if err = tx.Where("authority_id = ?", auth.AuthorityID).Delete(&[]system.SysAuthorityBtn{}).Error; err != nil {
+			return err
+		}
+		authorityID := strconv.Itoa(int(auth.AuthorityID))
+		if err = CasbinServiceApp.RemoveFilteredPolicy(tx, authorityID); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+// GetAuthorityInfoList
+// @function: GetAuthorityInfoList
+// @description: 分页获取数据
+// @param: info request.PageInfo
+// @return: list interface{}, total int64, err error
+func (authorityService *AuthorityService) GetAuthorityInfoList(info request.PageInfo) (list interface{}, total int64, err error) {
+	limit := info.PageSize
+	offset := info.PageSize * (info.Page - 1)
+	db := global.GVA_DB.Model(&system.SysAuthority{})
+	if err = db.Where("parent_id = ?", "0").Count(&total).Error; total == 0 || err != nil {
+		return
+	}
+	var authority []system.SysAuthority
+	err = db.Limit(limit).Offset(offset).Preload("DataAuthorityID").Where("parent_id = ?", "0").Find(&authority).Error
+	for k := range authority {
+		err = authorityService.findChildrenAuthority(&authority[k])
+	}
+	return authority, total, err
+}
+
+// GetAuthorityInfo
+// @function: GetAuthorityInfo
+// @description: 获取所有角色信息
+// @param: auth model.SysAuthority
+// @return: sa system.SysAuthority, err error
+func (authorityService *AuthorityService) GetAuthorityInfo(auth system.SysAuthority) (sa system.SysAuthority, err error) {
+	err = global.GVA_DB.Preload("DataAuthorityID").Where("authority_id = ?", auth.AuthorityID).First(&sa).Error
+	return sa, err
+}
+
+// SetDataAuthority
+// @function: SetDataAuthority
+// @description: 设置角色资源权限
+// @param: auth model.SysAuthority
+// @return: error
+func (authorityService *AuthorityService) SetDataAuthority(auth system.SysAuthority) error {
+	var s system.SysAuthority
+	global.GVA_DB.Preload("DataAuthorityID").First(&s, "authority_id = ?", auth.AuthorityID)
+	err := global.GVA_DB.Model(&s).Association("DataAuthorityID").Replace(&auth.SysBaseMenus)
+	return err
+}
+
+// SetMenuAuthority
+// @function: SetMenuAuthority
+// @description: 菜单与角色绑定
+// @param: auth *model.SysAuthority
+// @return: error
+func (authorityService *AuthorityService) SetMenuAuthority(auth *system.SysAuthority) error {
+	var s system.SysAuthority
+	global.GVA_DB.Preload("SysBaseMenus").First(&s, "authority_id = ?", auth.AuthorityID)
+	err := global.GVA_DB.Model(&s).Association("SysBaseMenus").Replace(&auth.SysBaseMenus)
+	return err
+}
+
+// findChildrenAuthority
+// @function: findChildrenAuthority
+// @description: 查询子角色
+// @param: authority *model.SysAuthority
+// @return: err error
+func (authorityService *AuthorityService) findChildrenAuthority(authority *system.SysAuthority) (err error) {
+	err = global.GVA_DB.Preload("DataAuthorityID").Where("parent_id = ?", authority.AuthorityID).Find(&authority.Children).Error
+	if len(authority.Children) > 0 {
+		for k := range authority.Children {
+			err = authorityService.findChildrenAuthority(&authority.Children[k])
+		}
+	}
+	return err
 }
