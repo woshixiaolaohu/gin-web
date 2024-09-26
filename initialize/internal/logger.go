05e5fd1780d4b48520bfffc5f190c9911ca4780a
jelly <huguodong1212@gmail.com>
2024-09-26 14:31:11 +0800

    1.补充部分代码

---
+++ initialize/internal/logger.go
@@ -0,0 +1,32 @@
+package internal
+
+import (
+	"fmt"
+	"gin-vue-admin/global"
+	"gorm.io/gorm/logger"
+)
+
+type writer struct {
+	logger.Writer
+}
+
+// NewWriter writer构造函数
+func NewWriter(w logger.Writer) *writer {
+	return &writer{Writer: w}
+}
+
+// Printf 格式化打印日志
+func (w *writer) Printf(message string, data ...interface{}) {
+	var logZap bool
+	switch global.GVA_CONFIG.System.DBType {
+	case "mysql":
+		logZap = global.GVA_CONFIG.Mysql.LogZap
+	case "pgsql":
+		logZap = global.GVA_CONFIG.Pgsql.LogZap
+	}
+	if logZap {
+		global.GVA_LOG.Info(fmt.Sprintf(message+"\n", data...))
+	} else {
+		w.Writer.Printf(message, data...)
+	}
+}
